415c074a41282299699cd1d5e533adff7143f207
pnguyen115 <49330551+pnguyen115@users.noreply.github.com>
2019-05-17 15:44:43 -0700

    Update inventory.cpp
    new update

--- inventory.cpp
+++ inventory.cpp
@@ -1,109 +1,137 @@
+// ConsoleApplication43.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 
 #include "pch.h"
 #include <iostream>
-#include <string>
 #include <iomanip>
-
+#include <fstream>
+#include <string>
 using namespace std;
-const double VALUE_OF_QUARTERS = 0.25;
-const double VALUE_OF_DIMES = 0.10;
-const double VALUE_OF_NICKELS = 0.05;
 
-class Machine
+
+// This program defines a class called Inventory that has itemnumber (which 
+// contains the id number of a product) and numofitem (which contains the 
+// quantity on hand of the corresponding product)as private data members.
+// The program will read these values from a file and store them in an 
+// array of objects (of type Inventory).  It will then print these values
+// to the screen.
+
+// Example: Given the following data file:
+//     986 8
+//     432 24
+// This program reads these values into an array of objects and prints the
+// following:
+//     Item number 986 has 8 items in stock
+//     Item number 432 has 24 items in stock
+
+
+const int NUMOFPROD = 23;   // This holds the number of products a store sells
+struct Items
+{
+	int id, quantity, price;
+	string menu;
+};
+class Inventory
 {
 private:
-	int q, d, n, dollar;
-	double initial_balance, current_balance;
+	Items inventory[NUMOFPROD];
 public:
-	Machine(int quaters = 0, int dimes = 0, int nickel = 0, int dollars = 0)
+	void setId(int code)
 	{
-		q = quaters;
-		d = dimes;
-		n = nickel;
-		dollar = dollars;
-		initial_balance = (VALUE_OF_QUARTERS * q) + (VALUE_OF_DIMES * d) + (VALUE_OF_NICKELS * n);
-	}
-	void print() const
-	{
-		cout << "Initial balance: " << initial_balance << "(" << dollar << " $, " << q << " Q, " << d << " D, " << n << " N)" << endl;
-	}
-};
-class A1 : public Machine
-{
-private:
-	int id[5] = { 1034,1000,1100,1123,1222 };
-	string code[5] = { "1A","1B","1C","1D","1E" };
-	string Decription[5]{ "Candy Bar", "Chocolate Chips", "Cookies", "Brownie", "Protein Bar" };
-	int quantities[5] = { 5,10,1,20,5 };
-public:
-	/*A1(int id1[], int code1[], string Nitem[], int quantity [])
-	{
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < NUMOFPROD; i++)
 		{
-			id[i] = id1[i];
-			code[i] = code1[i];
-			Decription[i] = Nitem[i];
-			quantities[i] = quantity[i];
+			inventory[i].id = code;
 		}
-	}*/
-	void subtract(string id)
+	}
+	void setAmount(int q)
 	{
-		int position;
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < NUMOFPROD; i++)
 		{
-			if (code[i] == id)
-			{
-				position = i;
-			}
+			inventory[i].quantity = q;
+		}
+	}
+	void setPrice(int price)
+	{
+		for (int i = 0; i < NUMOFPROD; i++)
+		{
+			inventory[i].price = price;
+		}
+	}
+	void setName(string name)
+	{
+		for (int i = 0; i < NUMOFPROD; i++)
+		{
+			inventory[i].menu = name;
+		}
+	}
+	int getId()
+	{
+		for (int i = 0; i < NUMOFPROD; i++)
+		{
+			return inventory[i].id;
+		}
+	}
+	int getAmount()
+	{
+		for (int i = 0; i < NUMOFPROD; i++)
+		{
+			return inventory[i].quantity;
+		}
+	}
+	int getPrice()
+	{
+		for (int i = 0; i < NUMOFPROD; i++)
+		{
+			return inventory[i].price;
+		}
+	}
+	string getName()
+	{
+		for (int i = 0; i < NUMOFPROD; i++)
+		{
+			return inventory[i].menu;
+		}
+	}
+	
+};
 
-		}
-		quantities[position]--;
-	}
-	void print() const
-	{
-		cout << "Code" << setw(8) << "Id" << setw(8) << "Description" << setw(8) << "Initial" << endl;
-		for (int i = 0; i < 5; i++)
-		{
-			cout << code[i] << setw(8) << id[i] << setw(8) << Decription[i] << setw(8) << quantities[i] << endl;
-		}
-	}
-};
+
 int main()
 {
-	cout << "Vending Machine System II" << endl;
-	Machine A1(2,4,4);
-	A1.print();
-	string code1, code2;
-	cout << "Please enter a startup code: ";
-	cin >> code1;
-	cout << "Initialzie machines. Please wait ... " << endl
-		<< "Machines are ready." << endl
-		<< "Available machines: 100A1, 100A2, 100B1, 100C1, 100C2 \n" << endl;
-	cout << "Select a machine: ";
-	cin >> code2;
-	while (code2 != code1)
+
+	ifstream infile;	      // Input file to read values into array
+	infile.open("D:\\CSCI 140\\Project 4\\products.txt");
+	if (!infile)
 	{
-		if (code2 == "100A1")
-		{
-			cout << "This machine accept one dollar bill only. \n"
-				<< "Available Items: \n"
-				<< "1A  50   Candy Bar \n"
-				<< "1B  35   Chocolate chips \n"
-				<< "1C  75   Cookies \n"
-				<< "1D  60   Brownie \n"
-				<< "1E  165  Protein Bar \n"
-				<< "Select an item: \n";
-			string select;
-			cin >> select;
-			
-
-
-			
-		}
-		cout << "Select a machine: ";
-		cin >> code2;
+		cout << "Error opening data file." << endl;
 	}
-	cout << "different" << endl;
+	else
+	{
+		Inventory products[NUMOFPROD];
+
+		int pos = 0;                   // loop counter
+		int id;                    // variable holding the id number
+		int q;
+		int price;
+		string name;// variable holding the total for each id number
+		while (pos < NUMOFPROD)
+		{
+			infile >> id >> q >> price;
+			getline(infile, name);
+			products[pos].setId(id);
+			products[pos].setAmount(q);
+			products[pos].setPrice(price);
+			products[pos].setName(name);
+			pos++;
+		}
+		for (int i = 0; i < NUMOFPROD; i++)
+		{
+			cout << products[i]->getId << products->getAmount << products->getPrice << products->getName << endl;
+		}
+
+	}
+	infile.close();
 	system("pause");
 	return 0;
+
 }
