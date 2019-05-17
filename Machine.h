97228d16e03a215ac486e8be5c73dcdab39cba34
jessmoody <49352744+jessmoody@users.noreply.github.com>
2019-05-17 16:23:19 -0700

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Machine
+{
+private:
+	string model;
+	struct Coins
+	{
+		int quarters;
+		int dimes;
+		int nickels;
+	};
+
+
+
+public:
+
+
+
+
+};
