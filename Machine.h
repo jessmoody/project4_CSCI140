b5ad888d39f1834d3755c765b4777ac106564648
jessmoody <49352744+jessmoody@users.noreply.github.com>
2019-05-20 21:15:52 -0700

    created outline for Machine class and subclasses

--- Machine.h
+++ Machine.h
@@ -1,3 +1,5 @@
+//outline of a potential class structure
+
 #pragma once
 
 #include <iostream>
@@ -7,20 +9,72 @@ using namespace std;
 
 class Machine
 {
-private:
+protected:
 	string model;
-	struct Coins
+	int amountEntered;
+	struct Coin
+	{
+		int quarters;
+		int dimes;
+		int nickels;
+	};
+	
+	// will add more variables as needed
+
+public:
+	Machine();
+	void setModel(string m);
+	// These are just my functions from Project 1 - some are modified
+	double calculateBalance(int totalCents);
+	bool validation(string userInput);
+	void print();
+	void printInstructions(double balance);
+	void printFinalReport(double balance, int totalCents);
+	// functions added for Project 4
+	int calculateTotalCents(Coin c);	// add up quarters, dimes and nickels
+	void getInventory();		// not actually void
+};
+
+class MachineA : public Machine
+{
+private:
+	struct Coin
 	{
 		int quarters;
 		int dimes;
 		int nickels;
 	};
 
-
-
 public:
+	MachineA();
+	bool makeChange(int amount, Coin &machine, Coin &back);
+	double calculateBalance(int totalCents, Coin c);
+	
+};
 
+class MachineB : public Machine
+{
+private:
+	struct Coin
+	{
+		int quarters;
+		int dimes;
+		int nickels;
+	};
+public:
+	MachineB();
+	bool makeChange(int amount, Coin &machine, Coin &back);
+	double calculateBalance(int totalCents);
+	
+};
 
-
-
+class MachineC : public Machine
+{
+private:
+	string cardEntered;
+public:
+	MachineC();
+	double calculateBalance(int totalCents);
+	bool validateCreditCard(string n);
+	
 };
