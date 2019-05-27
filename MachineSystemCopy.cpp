22e38e0dcff0269ad3cf88e64686faaa72fe791c
jessmoody <49352744+jessmoody@users.noreply.github.com>
2019-05-26 17:38:32 -0700

    Created MachineCopy and MachineSystemCopy
    Changed MachineSystem readFile() function so that it allocates memory within one loop.

---
+++ MachineSystemCopy.cpp
@@ -0,0 +1,104 @@
+#include "MachineCopy.h"
+#include "Inventory.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+//Inventory Product;
+
+void MachineSystem::setNewObject(Machine m, int index) // add object to array
+{
+	pM[index] = &m;
+	cout << m.getModel() << endl;
+
+	//system("pause");
+}
+
+void MachineSystem::readFile()
+{
+	ifstream infile;	      // Input file to read values into array
+	infile.open("C:\\Users\\jessm\\Documents\\GitHub\\project4_CSCI140\\machines.txt", ios::in);
+
+	string model;
+	int numMachines;
+	int quarters, dimes, nickels;
+	int numItems, itemID, quantity;		// amount of items of that type that can be put into the machine
+	string selection;
+	int count = 0;
+
+	// Change?
+	Inventory SystemProducts;
+
+	if (!infile)
+	{
+		cout << "Cannot access file" << endl;
+	}
+	else
+	{
+		while (infile >> model)
+		{
+			//Machine m;
+			//cout << "File accessed" << endl;
+			//m.setModel(model);
+			infile >> numMachines;
+			cout << "Model: " << model << endl << "Number of machines: " << numMachines << endl;
+			for (int i = 0; i < numMachines; i++)
+			{
+				//create machine
+
+				if (model == "100A")
+				{
+					pM[count] = new MachineA;
+				}
+				else if (model == "100B")
+				{
+					pM[count] = new MachineB;
+				}
+				else if (model == "100C")
+				{
+					pM[count] = new MachineC;
+				}
+				else
+				{
+					pM[count] = new Machine;
+
+				}
+
+				pM[count]->setModel(model);
+
+				infile >> quarters >> dimes >> nickels;
+				cout << "Quarters: " << quarters << endl;
+				cout << "Dimes: " << dimes << endl;
+				cout << "Nickels: " << nickels << endl;
+				cout << endl;
+				pM[count]->setCoins(quarters, dimes, nickels);
+
+				infile >> numItems;
+				pM[count]->setNumItems(numItems);
+
+				for (int i = 0; i < numItems; i++)
+				{
+					// create item object or struct
+
+					infile >> selection >> itemID >> quantity;
+
+					cout << "Selection: " << selection << endl;
+					cout << "Item ID: " << itemID << endl;
+					cout << "Quantity: " << quantity << endl << endl;
+
+					Items OneInventory = SystemProducts.checkOut(itemID, quantity);
+					//pM[count]->setProduct(selection, OneInventory, i);
+					pM[count]->setItem(selection, i, OneInventory);
+
+				}
+				//setNewObject(m, count);
+				count++;
+				totalMachines++;
+			}
+
+		}
+	}
+		infile.close();
+}
