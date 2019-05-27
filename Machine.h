4b639007ddeb603f98b18acb9275ccf0b2d484ef
jessmoody <49352744+jessmoody@users.noreply.github.com>
2019-05-26 18:37:53 -0700

    update Machine and MachineSystem files

--- Machine.h
+++ Machine.h
@@ -1,6 +1,8 @@
 //outline of a potential class structure
 
 #pragma once
+
+#include "Inventory.h"
 #include <iostream>
 #include <string>
 
@@ -12,7 +14,8 @@ class Machine
 {
 protected:
 	string model;
-	//int numMachines;
+	string machineName;
+	int numMachines;
 	struct Coins
 	{
 		int quarters;
@@ -28,11 +31,28 @@ protected:
 	};
 	OneItem items[SIZE];
 
+	struct OneProduct
+	{
+		string selection;
+		Items OneInventory;
+	};
+	OneProduct ProductList[SIZE];
+
 public:
-	Machine();
-	void setModel(string m){ model = m; }
-	
-	//void setNumMachines(int nm) { numMachines = nm; }
+	Machine()
+	{
+		printf("This is machine\n");
+	}
+
+	void setModel(string m)
+	{
+		model = m;
+	}
+	string getModel()
+	{
+		return model;
+	}
+	void setNumMachines(int nm) { numMachines = nm; }
 	void setCoins(int q, int d, int n)
 	{
 		inputCoin.quarters = q;
@@ -46,31 +66,84 @@ public:
 		items[n].itemID = id;
 		items[n].quantity = q;
 	}
-	string getModel() { return model; }
-	//int getNumMachines() { return numMachines; }
-	int getNumItems() { return numItems; }
-	int getQuarters()
+	void setItem(string s, int n, Items OneProd)
 	{
-		return inputCoin.quarters;
+		// Missing price and menu
+		items[n].itemID = OneProd.id;
+		items[n].quantity = OneProd.quantity;
+		items[n].selection = s;
 	}
-	virtual void printMachine();
+	void setProduct(string selection, Items OneInventory, int n)
+	{
+		ProductList[n].selection = selection;
+		ProductList[n].OneInventory = OneInventory;
+	}
+	void setMachineName();
+	void printMachine()
+	{
+		cout << "Machine Model: " << model << endl;
+		cout << "Quarters: " << inputCoin.quarters << endl;
+		cout << "Dimes: " << inputCoin.dimes << endl;
+		cout << "Nickels: " << inputCoin.nickels << endl;
+		cout << "Number of items: " << numItems << endl << endl;
+		cout << "Items: " << endl;
+		for (int i = 0; i < numItems; i++)
+		{
+			cout << "Selection: " << items[i].selection << endl
+				<< "Item ID: " << items[i].itemID << endl
+				<< "Quantity: " << items[i].quantity << endl << endl;
+		}
+	}
 
+	//// These are just my functions from Project 1 - some are modified
+	//double calculateBalance(int totalCents);
+	//bool validation(string userInput);
+	//void print();
+	//void printInstructions(double balance);
+	//void printFinalReport(double balance, int totalCents);
+	//// functions added for Project 4
+	//int calculateTotalCents(Coin c);	// add up quarters, dimes and nickels
+	//void getInventory();		// not actually void
 };
 
 class MachineA : public Machine
 {
 private:
+	struct Coin
+	{
+		int quarters;
+		int dimes;
+		int nickels;
+	};
 
 public:
-	MachineA() { ; }	
+	MachineA()
+	{
+		printf("This is machineA\n");
+	}
+	//bool makeChange(int amount, Coin &machine, Coin &back);
+	//double calculateBalance(int totalCents, Coin c);
+
 };
 
 class MachineB : public Machine
 {
 private:
-
+	struct Coin
+	{
+		int quarters;
+		int dimes;
+		int nickels;
+	};
 public:
-	MachineB() { ; }	
+	MachineB()
+	{
+		printf("This is machineB\n");
+
+	}
+	//bool makeChange(int amount, Coin &machine, Coin &back);
+	//double calculateBalance(int totalCents);
+
 };
 
 class MachineC : public Machine
@@ -78,38 +151,35 @@ class MachineC : public Machine
 private:
 	string cardEntered;
 public:
-	MachineC() { ; }
-	virtual void printMachine() override;
+	MachineC()
+	{
+		printf("This is machineC\n");
+
+	}
+	//double calculateBalance(int totalCents);
+	//bool validateCreditCard(string n);
+
 };
 
 class MachineSystem
 {
 protected:
-
-	Machine defaultMachine;
-	Machine *pDefault;
-	Machine * pM[SIZE] = { pDefault };
-	int totalMachines;
+	Machine * pM[SIZE];
+	int totalMachines = 0;
 
 public:
-	MachineSystem() { ; }
-	void setNewObject(Machine &m, int index);
-	void readFile(Machine *arr[]);
-	virtual void printAllMachines()
+	MachineSystem()
+	{
+		readFile();
+	}
+	void setNewObject(Machine m, int index);
+	void readFile();
+	void printAllMachines()
 	{
 		for (int i = 0; i < totalMachines; i++)
 		{
 			pM[i]->printMachine();
 		}
 	}
-	void setTotalMachines(int n)	{ totalMachines = n; }
-	int getTotalMachines() { return totalMachines; }
-	void setPtr(Machine *arr[], int n)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			pM[i] = arr[i];
-		}
-	}
-
+	~MachineSystem() { delete[] pM; }
 };
--- MachineSystem.cpp
+++ MachineSystem.cpp
@@ -1,29 +1,35 @@
-#include "Machine.h"
+#include "MachineCopy.h"
+#include "Inventory.h"
 #include <iostream>
 #include <string>
 #include <fstream>
 
 using namespace std;
 
-void MachineSystem::setNewObject(Machine &m, int index) // add object to array
+//Inventory Product;
+
+void MachineSystem::setNewObject(Machine m, int index) // add object to array
 {
 	pM[index] = &m;
 	cout << m.getModel() << endl;
+
+	//system("pause");
 }
 
-void MachineSystem::readFile(Machine *mPtr[])
+void MachineSystem::readFile()
 {
-	ifstream infile;
-
-	// filename based on computer used
+	ifstream infile;	      // Input file to read values into array
 	infile.open("C:\\Users\\jessm\\Documents\\GitHub\\project4_CSCI140\\machines.txt", ios::in);
 
 	string model;
 	int numMachines;
 	int quarters, dimes, nickels;
-	int numItems, itemID, quantity;
+	int numItems, itemID, quantity;		// amount of items of that type that can be put into the machine
 	string selection;
 	int count = 0;
+
+	// Change?
+	Inventory SystemProducts;
 
 	if (!infile)
 	{
@@ -31,30 +37,47 @@ void MachineSystem::readFile(Machine *mPtr[])
 	}
 	else
 	{
-		cout << "File accessed" << endl;
 		while (infile >> model)
 		{
-
+			//Machine m;
+			//cout << "File accessed" << endl;
+			//m.setModel(model);
 			infile >> numMachines;
-
 			cout << "Model: " << model << endl << "Number of machines: " << numMachines << endl;
-
 			for (int i = 0; i < numMachines; i++)
 			{
-				cout << model << endl;
-				mPtr[count]->setModel(model);
+				//create machine
+
+				if (model == "100A")
+				{
+					pM[count] = new MachineA;
+				}
+				else if (model == "100B")
+				{
+					pM[count] = new MachineB;
+				}
+				else if (model == "100C")
+				{
+					pM[count] = new MachineC;
+				}
+				else
+				{
+					pM[count] = new Machine;
+
+				}
+
+				pM[count]->setModel(model);
+				pM[count]->setMachineName(model);
+
 				infile >> quarters >> dimes >> nickels;
-				infile >> numItems;
-
-				mPtr[count]->setCoins(quarters, dimes, nickels);
-
-				//cout << "Quarters: " << mPtr[count]->getQuarters() << endl;
-				//cout << "Dimes: " << dimes << endl;
-				//cout << "Nickels: " << nickels << endl;
+				cout << "Quarters: " << quarters << endl;
+				cout << "Dimes: " << dimes << endl;
+				cout << "Nickels: " << nickels << endl;
 				cout << endl;
+				pM[count]->setCoins(quarters, dimes, nickels);
 
-
-				mPtr[count]->setNumItems(numItems);
+				infile >> numItems;
+				pM[count]->setNumItems(numItems);
 
 				for (int i = 0; i < numItems; i++)
 				{
@@ -62,33 +85,23 @@ void MachineSystem::readFile(Machine *mPtr[])
 
 					infile >> selection >> itemID >> quantity;
 
-					//cout << "Selection: " << selection << endl;
-					//cout << "Item ID: " << itemID << endl;
-					//cout << "Quantity: " << quantity << endl << endl;
+					cout << "Selection: " << selection << endl;
+					cout << "Item ID: " << itemID << endl;
+					cout << "Quantity: " << quantity << endl << endl;
 
-					mPtr[count]->setItem(selection, itemID, quantity, i);
+					// SystemProducts.getDescription(itemID);
+
+					Items OneInventory = SystemProducts.checkOut(itemID, quantity);
+					//pM[count]->setProduct(selection, OneInventory, i);
+					pM[count]->setItem(selection, i, OneInventory);
+
 				}
-				cout << "End of machine" << endl;
 				//setNewObject(m, count);
-
-				//cout << "Count: " << count << endl;
-
 				count++;
-
-
+				totalMachines++;
 			}
 
-			if (!infile)
-			{
-				cout << "End of file." << endl;
-			}
 		}
 	}
-
-	setPtr(mPtr, count);
-	setTotalMachines(count);
-
-	infile.close();
+		infile.close();
 }
-
-
