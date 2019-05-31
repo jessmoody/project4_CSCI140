886d739aedd9b6b25bd666e5d62254f554cd5343
jessmoody <49352744+jessmoody@users.noreply.github.com>
2019-05-31 15:30:39 -0700

    Moved MachineC functions to MachineC.cpp

--- Machine.h
+++ Machine.h
@@ -28,8 +28,6 @@ class Machine
 protected:
 	string model;
 	string machineName;
-	//int numMachines;
-
 	struct Coins
 	{
 		int quarters;
@@ -38,27 +36,24 @@ protected:
 	};
 	Coins inputCoin;
 	Coins currentCoin;
-
 	int initialDollars = 0;
 	int currentDollars = 0;
-
 	int transactions = 0;
 	double totalCost = 0; // In dollars
 	double initialBalance = 0;
 	double currentBalance = 0;
-
 	int numItems;
-
 	struct OneItem
 	{
 		string selection, description;
 		int itemID, initialQuantity, currentQuantity, price;
 	};
-
 	OneItem items[SIZE];
 
 public:
 	Machine();
+	// Destructor
+	// Open a file and record the machine information
 	~Machine();
 	void setModel(string m)
 	{
@@ -86,24 +81,9 @@ public:
 	}
 	virtual void printMachine();
 	void printAvailableItems();
-	// Destructor
-	// Open a file and record the machine information
 	
 	//TODO: remove this before submission	
-	void printInventory()
-	{
-		cout << "Machine inventory:\n"
-			<< "Code        Id       Description             Initial     Current" << endl;
-		for (int i = 0; i < numItems; i++)
-		{
-			cout << setw(4) << items[i].selection 
-				<< setw(10) << items[i].itemID << "       " 
-				<< left << setw(27) << items[i].description 
-				<< right << setw(2) << items[i].initialQuantity
-				<< setw(12) << items[i].currentQuantity << endl;
-		}
-	}
-	
+	void printInventory();
 };
 
 class MachineA : public Machine
@@ -122,8 +102,7 @@ public:
 		/*printf("This is machineA\n")*/;
 	}
 	//virtual void acceptMoney() override;
-	//bool makeChange(int amount, Coin &machine, Coin &back);
-	//double calculateBalance(int totalCents, Coin c);
+	
 
 };
 
@@ -143,8 +122,6 @@ public:
 
 	}
 	//virtual void acceptMoney() override;
-	//bool makeChange(int amount, Coin &machine, Coin &back);
-	//double calculateBalance(int totalCents);
 
 };
 
@@ -156,97 +133,11 @@ public:
 	MachineC()
 	{
 		/*printf("This is machineC\n")*/;
-
 	}
 	//double calculateBalance(int totalCents);
-	bool validateCreditCard(string n)
-	{
-		int size = n.length();
-
-		if (size > 19)	// Type cast to prevent overflow
-		{
-			return false;
-		}
-
-		// Sum of the even digits
-		int digit;
-		int sum = 0;    // sum of digits times two
-		for (int i = 1; i < size; i += 2)
-		{
-			// Add even digits
-			digit = n[size - 1 - i] - '0';
-			if (digit < 5)    // 2 * 5 = 10, we add 1 + 0 instead of 10
-			{
-				sum += digit * 2;
-			}
-			else
-			{
-				int digitTwo = digit * 2;
-				sum += digitTwo / 10 + digitTwo % 10;
-			}
-		}
-
-		// Add odd digits
-		for (int i = 0; i < size; i += 2)
-		{
-			digit = n[size - 1 - i] - '0';
-			sum += n[size - 1 - i] - '0';
-		}
-
-		// Validate
-		bool valid = (sum % 10 == 0) ? true : false;
-
-		return valid;
-	}
-
-	virtual void acceptMoney(int index) /*override*/
-	{
-		// Validate credit card
-		// Need to try different test cases:
-		// 1 fail 1 success, 
-		// 2 fails
-		string card;
-		bool valid;
-		int attempt = 0;
-
-		cout << "Enter your credit card number--> ";
-		cin >> card;
-		valid = validateCreditCard(card);
-		attempt++;
-		while (!valid)
-		{
-			cout << "Invalid credit card number was entered." << endl;
-
-			if (attempt == 2)	// After 2 fails
-			{
-				cout << "Too many invalid attempts.  Your selectionis cancelled." << endl;
-				return;
-			}
-
-			cout << "Enter your credit card number--> ";
-			cin >> card;
-			valid = validateCreditCard(card);
-			attempt++;
-		}
-
-		double priceDollar = (double)items[index].price / 100;
-		cout << "Your credit card was successfully charged for $" 
-			<< priceDollar << "." << endl;
-
-		// Change data in machine
-		transactions++;
-		items[index].currentQuantity--;
-		totalCost += priceDollar;
-		currentBalance += totalCost;
-
-		cout << "Thank you! Please take your item." << endl;
-
-	}
-
-	virtual double calcBalance(Coins c, int dollars)
-	{
-		return totalCost;
-	}
+	bool validateCreditCard(string n);
+	virtual void acceptMoney(int index); /*override*/
+	virtual double calcBalance(Coins c, int dollars);
 };
 
 class MachineSystem
